215d90d9b56975d87ac40ba06f7c49209ce660a1
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-06-23 11:14:03 +0530

    create a mongodb atlas and connect to my server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express=require('express')
 require('dotenv').config()
+const connectDb=require('./confiq/db')
 
 const app=express()
 const server_port=process.env.SERVER_PORT
+connectDb()
 
 app.get("/api",(req,res)=>{
     res.json("hello guys")
