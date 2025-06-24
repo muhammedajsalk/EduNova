d4a4314e5df705516ecf357b999406986fce1420
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-06-25 00:51:35 +0530

    the reset password api is created

---
+++ server/controllers/users controller/resetPassword.js
@@ -0,0 +1,34 @@
+const userModel = require("../../models/usersModel")
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs')
+require('dotenv').config()
+
+
+async function resetPassword(req, res) {
+    try {
+        const { token } = req.query
+        console.log(req.query)
+        if (!token) return res.status(404).json({ success: false, message: "token is missing" })
+        const { password } = req.body
+        jwt.verify(token, process.env.JWT_SECRET_CODE,(error,decode)=>{
+           if(error) return res.status(404).json({ success: false, message: "Invalid or expired token" })
+           req.user=decode
+        })
+        const user = await userModel.findById(req.user.id)
+        if (!user || user.resetToken !== token || user.resetTokenExpiry < Date.now()) {
+            return res.status(400).json({ message: "Invalid or expired token" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        user.password = hashedPassword
+        user.resetToken = null
+        user.resetTokenExpiry = null
+        await user.save()
+        res.status(200).json({ success: true, message: "Password reset successful. You can now log in." })
+    } catch (err) {
+        res.status(400).json({ message: "server side error" });
+        console.log(err)
+    }
+
+}
+
+module.exports=resetPassword
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -5,12 +5,14 @@ const otpSent = require('../controllers/users controller/otpGenerator')
 const otpAuth = require('../middlewares/nodeMailerAuth')
 const login = require('../controllers/users controller/login')
 const forgetPassword = require('../controllers/users controller/forgetPassword')
+const resetPassword = require('../controllers/users controller/resetPassword')
 
 
 router.post("/auth/register",otpAuth,userRegister)
 router.post("/auth/otpSent",otpSent)
 router.post("/auth/login",login)
 router.post('/auth/forgetPassword',forgetPassword)
+router.post('/auth/resetPassword',resetPassword)
 
 
 module.exports=router
