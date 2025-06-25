e09133643125f5747d5eff88e7c945980db17e69
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-06-25 19:54:43 +0530

    Setup Multer + Cloudinary Storage

---
+++ server/middlewares/instructor/cloudinaryUploader.js
@@ -0,0 +1,22 @@
+const multer = require('multer')
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const cloudinary = require('../../confiq/cloudinary')
+
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: async (req, res) => {
+        const instructorName = req.body.name?.replace(/\s+/g, '_').toLowerCase();
+        const folder = `eduNova/instructors/${instructorName || 'unknown'}`;
+        let resource_type = file.mimetype.startsWith('video') ? 'video' : 'auto';
+        return {
+            folder,
+            resource_type,
+            public_id: `${Date.now()}-${file.originalname}`, // Unique file name
+        };
+    }
+})
+
+const upload=multer({storage})
+
+module.exports=upload
