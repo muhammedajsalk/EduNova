3d3fd0011256d1673eb64dba70f7f0fa5f095bdc
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-10-11 20:15:57 +0530

    fix:change the cors

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ connectDb();
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://edunovas.vercel.app", credentials: true }));
 
 startCronJobs()
 
