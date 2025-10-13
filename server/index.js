986f971087dbda53ec1f5634fa9a2f5abc8e25ba
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-10-13 22:28:21 +0530

    fix:the index js fie=xed ines more

--- server/index.js
+++ server/index.js
@@ -21,10 +21,13 @@ const server = http.createServer(app);
 
 connectDb();
 
+
+app.set('trust proxy', 1)
+app.use(cors({ origin: "https://edunovas.vercel.app", credentials: true }));
+
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cookieParser());
-app.use(cors({ origin: "https://edunovas.vercel.app", credentials: true }));
 
 startCronJobs()
 
