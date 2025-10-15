7643c23ab20929b05376d396b4edc7ce0532eceb
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-10-15 23:29:12 +0530

    fix:the nodemailer

--- server/controllers/users controller/otpGenerator.js
+++ server/controllers/users controller/otpGenerator.js
@@ -4,11 +4,15 @@ const nodemailer = require("nodemailer");
 require('dotenv').config()
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // Use STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    logger: true, // Enable logging
+    debug: true, // Detailed SMTP logs
 });
 
 async function otpSent(req, res, next) {
